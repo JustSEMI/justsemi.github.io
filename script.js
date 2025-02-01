f45c5ea7f469d8556008bd8473d890e3cfbc69bd
JustSEMI <smuel.fht@gmail.com>
2025-02-01 17:42:01 +0700

    Deploy first Website

---
+++ script.js
@@ -0,0 +1,12 @@
+function updateTime() {
+	let now = new Date();
+	let hours = now.getHours().toString().padStart(2, '0');
+	let minutes = now.getMinutes().toString().padStart(2, '0');
+	let seconds = now.getSeconds().toString().padStart(2, '0');
+
+	document.getElementById('hours').textContent = hours;
+	document.getElementById('minutes').textContent = minutes;
+	document.getElementById('seconds').textContent = seconds;
+}
+
+setInterval(updateTime, 1000);
